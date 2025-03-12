00c78d9430ad8094bba5d07dbf156c35647b5976
firleichjordan <jordanhombing.js@gmail.com>
2025-03-12 22:14:29 +0700

    fix: home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,27 @@
 module.exports = {
   content: ["index.html"],
   darkMode: "class",
-  theme: {
-    container: {
-      center: true,
-      padding: "16px",
-    },
-    extend: {
-      colors: {
-        primary: "#14b8a6",
-        secondary: "#64748b",
-        dark: "#0f172a",
-      },
-      screens: {
-        "2xl": "1320px",
-      },
-    },
-  },
+  // theme: {
+  //   container: {
+  //     center: true,
+  //     padding: "16px",
+  //   },
+  //   extend: {
+  //     colors: {
+  //       primary: "#14b8a6",
+  //       secondary: "#64748b",
+  //       dark: "#0f172a",
+  //     },
+  //     width: {
+  //       circle: "50px",
+  //     },
+  //     height: {
+  //       circle: "50px",
+  //     },
+  //     screens: {
+  //       "2xl": "1320px",
+  //     },
+  //   },
+  // },
   plugins: [],
 };
